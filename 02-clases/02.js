ebdc880b0646787e6d89ddfe865964eb19b91113
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-03-16 20:28:46 -0600

    MANEJO DE CLASES CON VUE

---
+++ 02-clases/02.js
@@ -0,0 +1,13 @@
+// instanciamos Vue.js
+const app = new Vue({
+    // declaramos que la aplicación estará en el
+    // elemento con el ID app
+    el: '#app',
+    data: {
+        fondo: 'bg-warning',
+        color: false
+    },
+    methods: {
+
+    }
+})
