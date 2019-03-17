b298cdae419b55cd34f41458cf2697803fe224fe
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-03-16 20:11:01 -0600

    COMPUTED

--- 01-introduccion/01.js
+++ 01-introduccion/01.js
@@ -10,15 +10,27 @@ const app = new Vue({
             { nombre: 'Pera', cantidad: 0 }, 
             { nombre: 'Platano', cantidad: 11 }, 
         ],
-        nuevaFruta: ''
+        nuevaFruta: '',
+        total: 0
     },
     methods: {
-        // agregarFruta: function(){}
         agregarFruta(){
             this.frutas.push({
                 nombre: this.nuevaFruta,
                 cantidad: 0
-            })
+            });
+
+            this.nuevaFruta = '';
         },
+    },
+    computed: {
+        sumarFrutas(){
+            this.total = 0;
+            for(fruta of this.frutas){
+                this.total = this.total + fruta.cantidad;
+            }
+
+            return this.total;
+        }
     }
 })
